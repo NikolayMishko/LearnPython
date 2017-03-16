c81dd1bbf8035bd3ab32a9831f245d20f5bc532b
Nikolay Mishko <nickmishko@gmail.com>
2017-03-16 17:36:36 +0300

    Home work

--- dialog.py
+++ dialog.py
@@ -3,8 +3,13 @@ def get_answer():
 	print('Давай сначала разберемся все ли у тебя хорошо')
 
 def ask_user(answer):
-	while answer!='Хорошо':
-		answer=input('Как дела? ')
-		if answer[-1]=='?':
-			get_answer()
+	try:
+		while answer!='Хорошо':
+			answer=input('Как дела? ')
+			if answer=='':
+				print('Что молчишь?')
+			elif answer[-1]=='?':
+				get_answer()	
+	except KeyboardInterrupt:
+		return print("\nЛадно, ладно, только без рук, я же шучу")			
 ask_user(answer)
