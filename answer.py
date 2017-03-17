01edf10e79091356331771d4956573dfc5cf6b7b
NikolayMishko <nickmishko@gmail.com>
2017-03-17 10:08:06 +0300

    Delete answer.py

--- answer.py
+++
@@ -1,9 +0,0 @@
-dialog1=' '
-def get_answer(question):
-	answers={"привет":"И тебе привет!"," как дела ?":" Лучше всех","пока":" Увидимся"}
-	return answers.get(question.lower(),'привет, попробуй еще раз')
-while dialog1!='пока':	
-	dialog1=input()
-	print(get_answer(dialog1))
-print('Привет GitHub')
-
