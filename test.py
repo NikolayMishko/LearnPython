0e216a2b321e2277615dcac478c9a7320cbeecd1
NikolayMishko <nickmishko@gmail.com>
2017-03-17 10:06:04 +0300

    Delete test.py

--- test.py
+++
@@ -1,5 +0,0 @@
-something=input(str('Введите первую пременную '))
-something_else=input(str('Введите вторую переменную '))
-def summ_something(soтеthing,something_else):
-	return something.upper+something_else
-print(summ_something(something,something_else))
