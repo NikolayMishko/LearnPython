86cee01e218f32f9ac7c474407142f7f81352f03
NikolayMishko <nickmishko@gmail.com>
2017-03-17 10:03:30 +0300

    Add files via upload

---
+++ comparison_st.py
@@ -0,0 +1,10 @@
+string1=str(input('Введите строку 1 '))
+string2=str(input('Введите строку 2 '))
+def string_comparison(string1,string2):
+	if string1==string2:
+		return('1')
+	if string1!=string2 and (len(string1)>len(string2)):
+		return('2')
+	if string1!=string2 and string2=='learn':
+		return('3')
+print(string_comparison(string1,string2))
