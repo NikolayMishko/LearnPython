88daa3516d0a8440f11e867c310f30e670252870
NikolayMishko <nickmishko@gmail.com>
2017-03-17 10:07:15 +0300

    Delete hello.py

--- hello.py
+++
@@ -1,6 +0,0 @@
-#name='Коля'
-#print('Привет, '+name)
-user_info={'first name': '','last name': ''}
-user_info['first name']=input('Введите ваше имя ')
-user_info['last name']=input('Введите вашу фамилию ')
-print('Привет, '+user_info['first name']+' '+user_info['last name'])
